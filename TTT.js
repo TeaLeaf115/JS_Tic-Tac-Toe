5b7f841dcad4f2b6711a5299530f3e904590b3e7
TeaLeaf115 <91578545+TeaLeaf115@users.noreply.github.com>
2022-06-25 13:51:37 -0400

    Added VGL (See extended description)
    Added visual game logic when adding the X's and O's onto the board as well as implementing the groundwork for actual game logic so the game can be playable.

--- TTT.js
+++ TTT.js
@@ -12,11 +12,13 @@ const POS_TO_PX = new Map([
     
     
 var board = new Grid(3, 3);
+board.init(0);
 
 function start()
 {
 	drawGame();
     mouseClickMethod(onClick);
+    
 }
 
 function drawGame()
@@ -43,22 +45,64 @@ function onClick(m)
     var mY = m.getY();
     
     const IN_TOP_ROW = mY < 100;
-    const IN_MID_ROW = mY >= 100 && mY <= 200;
+    const IN_MID_ROW = mY > 100 && mY < 200;
     const IN_BOT_ROW = mY > 200;
     
-    const IN_LEFT_COL mX < 100;
-    const IN_MID_COL = mX >= 100 && mY <= 200;
-    const IN_RIGHT_COL mX > 200;
+    const IN_LEFT_COL = mX < 100;
+    const IN_MID_COL = mX > 100 && mX < 200;
+    const IN_RIGHT_COL = mX > 200;
     
     
+    
+    if (IN_TOP_ROW && IN_LEFT_COL)
+    { takeTurn(0, 0); }
+    
+    else if (IN_TOP_ROW && IN_MID_COL)
+    { takeTurn(1, 0); }
+    
+    else if (IN_TOP_ROW && IN_RIGHT_COL)
+    { takeTurn(2, 0); }
+    
+    
+    
+    else if (IN_MID_ROW && IN_LEFT_COL)
+    { takeTurn(0, 1); }
+    
+    else if (IN_MID_ROW && IN_MID_COL)
+    { takeTurn(1, 1); }
+    
+    else if (IN_MID_ROW && IN_RIGHT_COL)
+    { takeTurn(2, 1); }
+    
+    
+    
+    else if (IN_BOT_ROW && IN_LEFT_COL)
+    { takeTurn(0, 2); }
+    
+    else if (IN_BOT_ROW && IN_MID_COL)
+    { takeTurn(1, 2); }
+    
+    else if (IN_BOT_ROW && IN_RIGHT_COL)
+    { takeTurn(2, 2); }
     
 }
 
+function takeTurn(row, col)
+{
+    if (board.get(row, col) != 0)
+    { return; }
+    
+    if (getTurnVal() == 0)
+    { drawX(row, col); }
+    
+    else
+    { drawO(row, col); }
+}
 
 // This draws an X at a certain location on the screen
 function drawX(row, col)
 {
-    var x = new WebImage("https://github.com/TeaLeaf115/JS_Tic-Tac-Toe/blob/main/JS-TTT_X.png?raw=true");
+    var x = new WebImage("https://raw.githubusercontent.com/TeaLeaf115/JS_Tic-Tac-Toe/main/JS-TTT_X.png");
     
     x.setPosition(POS_TO_PX.get(row), POS_TO_PX.get(col));
     
@@ -67,12 +111,15 @@ function drawX(row, col)
     add(x);
     
     board.set(row, col, 1);
+    print(board);
+    
+    turn++;
 }
 
 // This draws an O at a certain location on the screen
 function drawO(row, col)
 {
-    var o = new WebImage("https://github.com/TeaLeaf115/JS_Tic-Tac-Toe/blob/main/JS-TTT_O.png?raw=true");
+    var o = new WebImage("https://raw.githubusercontent.com/TeaLeaf115/JS_Tic-Tac-Toe/main/JS-TTT_O.png");
     
     o.setPosition(POS_TO_PX.get(row), POS_TO_PX.get(col));
     
@@ -81,10 +128,7 @@ function drawO(row, col)
     add(o);
     
     board.set(row, col, 2);
-}
-
-
-function xoPlacement(e)
-{
+    print(board);
     
+    turn++;
 }
